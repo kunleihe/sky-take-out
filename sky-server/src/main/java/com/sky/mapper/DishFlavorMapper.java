00f5826f1a6eb8374a68791086f13b715988e89b
Kunlei He <kunleih@uci.edu>
2026-01-07 19:58:06 -0800

    feat: delete and update dish

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,4 +15,21 @@ public interface DishFlavorMapper {
      * @param flavors
      */
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品id删除对应的口味数据
+     *
+     * @param dishId
+     */
+    @Delete("DELETE FROM dish_flavor WHERE dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品id查询对应的口味
+     *
+     * @param dishId
+     * @return
+     */
+    @Select("SELECT * FROM dish_flavor where dish_id = #{dish_id}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
