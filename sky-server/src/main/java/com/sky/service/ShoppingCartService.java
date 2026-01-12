575eb522df2cdfae41432e7b66d9ab91c5a6918e
Kunlei He <kunleih@uci.edu>
2026-01-12 15:46:29 -0800

    feat: clean shopping cart

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -19,4 +19,10 @@ public interface ShoppingCartService {
      * @return
      */
     List<ShoppingCart> showShoppingCart();
+
+    /**
+     * 清空购物车商品
+     *
+     */
+    void cleanShoppingCart();
 }
