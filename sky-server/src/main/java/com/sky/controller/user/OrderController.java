9615a01d0b15b522a60d20834f65907585110923
Kunlei He <kunleih@uci.edu>
2026-01-12 16:15:09 -0800

    feat: submit order

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/order")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 用户下单
+     *
+     * @param orderSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    @ApiOperation("用户下单")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO orderSubmitDTO) {
+        log.info("用户下单：{}", orderSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(orderSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
